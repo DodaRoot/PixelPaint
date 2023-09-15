6e1dfdc2e86cc37535a552d40e69fdd31c9faf9b
Doda <root.doda@gmail.com>
2023-09-15 19:11:41 +0200

    Added colors erase clear and pen mode

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // Global Variables
-let pixels = 18;
+let pixels = 7;
 let round = false;
+let color = 'black'
+let input = document.querySelector('input')
 
 // Linking the DOM
 const container = document.querySelector('.container')
-let add = document.getElementById('add')
 
 // Loop to add pixels and call beginning functions
 function start () {   
@@ -26,10 +27,36 @@ function makeRow () {
 function makeCel (madeRow) {
     const madeCel = document.createElement('div')
     madeCel.classList.add('madeCell')
+    madeCel.addEventListener('mousedown', () => {
+        madeCel.style.backgroundColor = color
+    })
     madeRow.appendChild(madeCel)
 }
 
+function add () {
+    container.innerHTML = ''
+    pixels += 2
+    start()
+}
+
+function subtract () {
+    container.innerHTML = ''
+    pixels -= 2
+    start()
+}
+
+function clearAll () {
+    container.innerHTML = ''
+    start()
+}
+
+function erase () {
+    color = 'white'
+}
 
 start()
 
+input.addEventListener('input', () => {
+    color = input.value
+})
 
