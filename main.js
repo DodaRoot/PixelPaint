2f3e36a5bd2b38c1b8c32964ce213893af4395bf
Doda <root.doda@gmail.com>
2023-09-15 16:10:37 +0200

    Added the basic grid logic and made the pixels

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 // Global Variables
-let pixels = 16;
+let pixels = 18;
+let round = false;
 
 // Linking the DOM
-let container = document.querySelector('.container')
-let madeRow = document.createElement('div')
-let madeCel = document.createElement('div')
-madeRow.classList.add('madeRow')
-madeCel.classList.add('madeCell')
+const container = document.querySelector('.container')
+let add = document.getElementById('add')
 
-function makeRow (madeRow) {
-    container.appendChild(madeRow)
+// Loop to add pixels and call beginning functions
+function start () {   
+    for (let p = 0; p < pixels; p++) {
+        makeRow()
+    } 
 }
 
+
+// Beginning logic functions to add div's
+function makeRow () {
+    const madeRow = document.createElement('div')
+    madeRow.classList.add('madeRow')
+    container.appendChild(madeRow)
+    for (let i = 0; i < pixels; i++) {
+        makeCel(madeRow)
+    }
+}
 function makeCel (madeRow) {
+    const madeCel = document.createElement('div')
+    madeCel.classList.add('madeCell')
     madeRow.appendChild(madeCel)
 }
 
-makeRow(madeRow)
-makeCel(madeRow)
 
+start()
 
 
