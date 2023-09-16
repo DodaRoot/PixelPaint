2e68c644be2e002753b7be237e85d40a0a28a365
Doda <root.doda@gmail.com>
2023-09-16 14:39:03 +0200

    Added random colors

--- main.js
+++ main.js
@@ -49,26 +49,75 @@ function subtract () {
 }
 
 function clearAll () {
+    randomColor = false;
     container.innerHTML = ''
     start()
 }
 
 function erase () {
+    randomColor = false;
     color = 'white'
 }
 
 start()
 
 input.addEventListener('input', () => {
+    randomColor = false;
     color = input.value
 })
 
+let randomColor = false;
+let colorRandom = '';
+
+function random () {
+    randomColor = true;
+}
 
 // Had to look this one up its a function to draw when clicking
 // and holding the mouse over the div's
 function pointer (e) {
     if (e.type === 'mouseover' && !mouse) return
+    else if (randomColor === true) {
+        let randomNum = Math.floor(Math.random() * 10)
+        colors(randomNum)
+        e.target.style.backgroundColor = colorRandom;
+    }
     else {
         e.target.style.backgroundColor = color;
+    }
+}
+
+function colors (randomNum) {
+    switch(randomNum) {
+        case 0
+        :colorRandom = 'aqua'
+        break
+        case 1
+        :colorRandom = 'red'
+        break
+        case 2
+        :colorRandom = 'green'
+        break
+        case 3
+        :colorRandom = 'black'
+        break
+        case 4
+        :colorRandom = 'blue'
+        break
+        case 5
+        :colorRandom = 'yellow'
+        break
+        case 6
+        :colorRandom = 'violet'
+        break
+        case 7
+        :colorRandom = 'pink'
+        break
+        case 8
+        :colorRandom = 'grey'
+        break
+        case 9
+        :colorRandom = 'gold'
+        break
     }
 }
